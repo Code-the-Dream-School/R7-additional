2993de942d853c44e963a5196594f70be562db77
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-12 13:40:35 -0500

    debugger

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'customers#index'
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
