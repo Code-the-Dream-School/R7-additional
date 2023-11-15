e49f908e3c2c69eacaa9668d507ff50baf0f59e6
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-14 20:32:33 -0500

    Exception handling

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :catch_not_found
   before_action :set_customer, only: %i[ show edit update destroy ]
 
   # GET /customers or /customers.json
@@ -68,4 +69,9 @@ class CustomersController < ApplicationController
     def customer_params
       params.require(:customer).permit(:first_name, :last_name, :phone, :email)
     end
+    def catch_not_found(e)
+      Rails.logger.debug("We had a not found exception.")
+      flash.alert = e.to_s
+      redirect_to customers_path
+end
 end
