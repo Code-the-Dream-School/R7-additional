a187dc3fee78e131eff94e79090fb39914b9ef60
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-21 12:14:36 -0500

    complete assignment

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -19,10 +19,18 @@ RSpec.describe "CustomersControllers", type: :request do
     end
   end
 describe "get new_customer_path" do
-    it "renders the :new template"
+    it "renders the :new template" do
+        customer = FactoryBot.create(:customer)
+      get new_customer_path(id: customer.id)
+      expect(response).to render_template(:new)
+    end
   end
   describe "get edit_customer_path" do
-    it "renders the :edit template"
+    it "renders the :edit template" do
+          customer = FactoryBot.create(:customer)
+      get edit_customer_path(id: customer.id)
+      expect(response).to render_template(:edit)
+      end
   end
   describe "post customers_path with valid data" do
     it "saves a new entry and redirects to the show path for the entry" do
@@ -42,26 +50,29 @@ describe "get new_customer_path" do
     end
   end
   describe "put customer_path with valid data" do
-    it "updates an entry and redirects to the show path for the customer"
+    it "updates an entry and redirects to the show path for the customer" do
+      customer = FactoryBot.create(:customer)
+      put customer_path(customer.id), params: {customer: {phone: "1231231230"}}                 
+      customer.reload
+      expect(customer.phone).to eq("1231231230")
+      expect(response).to redirect_to customer_path(customer.id)
+    end
   end
   describe "put customer_path with invalid data" do
-    it "does not update the customer record or redirect"
+    it "does not update the customer record or redirect" do
+      customer = FactoryBot.create(:customer)
+      put customer_path(customer.id), params: {customer: {phone: "123"}}
+      customer.reload
+      expect(customer.phone).not_to eq("123")
+      expect(response).to render_template(:edit) 
+    end
   end
   describe "delete a customer record" do
-    it "deletes a customer record"
+    it "deletes a customer record" do
+      customer = FactoryBot.create(:customer)
+       expect { delete  customer_path(customer.id)
+    }.to change(Customer, :count)
+      expect(response).to redirect_to customers_path
+    end
   end
 end
-
-
-
-
-# require 'rails_helper'
-
-# RSpec.describe "Customers", type: :request do
-#   describe "GET /customers" do
-#     it "works! (now write some real specs)" do
-#       get customers_index_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
