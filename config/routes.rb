873a1d6f4cb0b5e974401ed8eeb807fcd6b9e76e
Desiree Mcclain <desireemcclain51@gmail.com>
2024-08-14 18:24:51 -0400

    lesson 8 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+root to: 'customers#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
